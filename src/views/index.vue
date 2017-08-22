90e6ba708cd7269481caf4c332a872562cd5fb6f
zhouln <zhouln@senthink.com>
2017-08-22 09:03:40 +0800

    项目

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@
   <header>
     <div class="header">
       <h2>场地类型</h2>
-      <div class="head_right"> <a class="user" href="record.html"> </a> </div>
+      <div class="head_right"> <a class="user" href="record"> </a> </div>
     </div>
   </header>
   <!--header 结束--> 
@@ -13,24 +13,24 @@
   	<a href="tel:+15757856627" class="phone-line"><i class="icon-phone"></i>15757856627</a>
   	<div class="qiuc">
   		<img src="../assets/img/CDtaiQ.png">
-  		<router-link class="changdi" to="booking">
+  		<a class="changdi" href="booking">
   			<h3>会议室</h3>
   			<p>开放时间：9:00-24:00</p>
-		</router-link>
+  		</a>
   	</div>
   	<div class="qiuc">
   		<img src="../assets/img/CDyuMao.png">
-  		<router-link class="changdi" to="booking">
+  		<a class="changdi" href="booking">
   			<h3>篮球场</h3>
   			<p>开放时间：9:00-24:00</p>
-		</router-link>
+  		</a>
   	</div>
   	<div class="qiuc">
   		<img src="../assets/img/CDpingP.png">
-  		<router-link class="changdi" to="booking">
+  		<a class="changdi" href="booking">
   			<h3>瑜伽房</h3>
   			<p>开放时间：9:00-24:00</p>
-  		</router-link>
+  		</a>
   	</div>
   </div>
   <!--main-->
